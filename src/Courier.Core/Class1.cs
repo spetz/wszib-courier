f8bcd4bbc49363901c1f56acb6846542f89ed709
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-03 09:18:28 +0100

    Init

---
+++ src/Courier.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Courier.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/Courier.Tests.EndToEnd/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Courier.Tests.EndToEnd
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
