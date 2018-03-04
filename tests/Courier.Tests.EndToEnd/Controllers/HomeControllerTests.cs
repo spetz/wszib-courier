15a27f16f8de9cb9e988280ea54aad4044d07292
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-04 15:36:16 +0100

    Home controller E2E test

---
+++ tests/Courier.Tests.EndToEnd/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Courier.Api;
+using FluentAssertions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace Courier.Tests.EndToEnd.Controllers
+{
+    public class HomeControllerTests
+    {
+        TestServer Server;
+        HttpClient HttpClient;
+
+        public HomeControllerTests()
+        {
+            Server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            HttpClient = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task home_controller_get_should_return_string_content()
+        {
+            var response = await HttpClient.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Be("Welcome to Courier API.");
+        }
+    }
+}
